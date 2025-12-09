bfb3e2682b121fc3b58da8b6ae41229ad31a0caa
yunseon <husu7400@cau.ac.kr>
2025-12-09 10:35:56 +0900

    Refactor subtraction and add multiplication calculation

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ num1 = int(first)
 num2 = int(second)
 
 # 차 계산
-result = num1 - num2
+difference = num1 - num2
+
+# 곱 계산
+product = num1 * num2
 
 # 결과 출력
-print("Difference: ", result)
+print("Difference: ", difference)
+print("Product: ", product)
